986cebfdefbde3e77c70560ad36b76fe475874eb
etienne divet <edivet@csumb.edu>
2018-02-07 16:22:42 +0000

    added index

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home page for Git merge </title>
+    </head>
+    <body>
+        Hopefully we learn how to merge the hard way, today
+    </body>
+</html>
