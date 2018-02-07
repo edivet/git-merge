3f72c9e9bd263eceabc4cf7e789003d0cb73da10
etienne divet <edivet@csumb.edu>
2018-02-07 16:30:04 +0000

    changed title to dashboard

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Home page for Git merge </title>
+        <title>Dashboard for Git merge </title>
     </head>
     <body>
         Hopefully we learn how to merge the hard way, today
